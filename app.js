cc2704c93dbf70e9c05c3e38f52519956003a06e
王希宁 <xiningwang120@gmail.com>
2018-01-15 10:23:12 -0600

    node std input start

---
+++ app.js
@@ -0,0 +1,17 @@
+console.log('Starting app.js');
+
+const fs = require('fs');
+const _ = require('lodash');
+
+const notes = require('./notes.js');
+
+var command = process.argv[2];
+console.log('Command: ', command);
+
+if (command === 'add') {
+    console.log('Adding new note');
+} else if (command === 'list') {
+    console.log('Listing all notes');
+} else {
+    console.log('Not recognized');
+}
---
+++ notes.js
@@ -0,0 +1,11 @@
+console.log('Starting notes.js');
+
+var addNote = () => {
+    console.log('addNote');
+    return 'New Note';
+};
+
+
+module.exports = {
+    addNote
+}
