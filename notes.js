add250d705d247cf90e5bbaddd9c750c2aa6fcad
王希宁 <xiningwang120@gmail.com>
2018-01-16 10:22:19 -0600

    complete add note functionality

--- notes.js
+++ notes.js
@@ -1,10 +1,37 @@
 console.log('Starting notes.js');
 
+const fs = require('fs');
+
+var fetchNotes = () => {
+    try {
+        var notesString = fs.readFileSync('notes-data.json');
+        return JSON.parse(notesString);
+    } catch (e) {
+        return [];
+    }
+};
+
+var saveNote = (notes) => {
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+};
+
 var addNote = (title, body) => {
-    console.log('Adding note', title, body);
+    var notes = fetchNotes();
+    var note = {
+        title,
+        body
+    };
+    var duplicateNotes = notes.filter(note => note.title === title);
+
+    if (duplicateNotes.length === 0) {
+        notes.push(note);
+        saveNote(notes);
+        return note;
+    }
 };
 
 var getAll = () => {
+    // var notesString = fs.readFileSync('notes-data.json', )
     console.log('Getting all notes');
 };
 
